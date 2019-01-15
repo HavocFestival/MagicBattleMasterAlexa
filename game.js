3aacd75c96db8abb8e8f3c9e90d327c873de5a64
Wolfknightx <Wolfknightx@Gmail.com>
2019-01-15 12:49:08 -0600

    working out an example of exporting info from another file.

--- game.js
+++ game.js
@@ -1,72 +1,74 @@
-
-class ListObject {
-    constructor(data){
-        this.Name = FormatString(data.Name);
-        this.Description = FormatString(data.Description);
-        this.GameRules = FormatString(data.GameRules);
-        this.IsSupplemental = FormatBool(data.IsSupplemental);
-        this.IsAddon = FormatBool(data.IsAddon);
-        this.Type = FormatString(data.Type);    
-    }
-}
-
-class MagicTerm {
-    constructor(data) {
-        this.Name = FormatString(data.Name);
-        this.Description = FormatString(data.Description);
-    }
-}
-
-class RollingChart {
-    constructor(data) {
-        this.Id = data.Id;
-        this.Name = FormatString(data.Name);
-        this.Chance = data.Chance;
-        this.ChartType = FormatString(data.ChartType);
-        this.Announcement = FormatString(data.Announcement);
-        this.IsAddon = FormatBool(data.IsAddon);
-        this.IsSupplemental = FormatBool(data.IsSupplemental);
-        this.IsRollAgain = FormatBool(data.IsRollAgain);
-    }
-}
-
-class RandomChart {
-    constructor(data) {
-        this.Id = data.Id;
-        this.Announcement = FormatString(data.Announcement);
-        this.Chance = data.Chance;
-        this.Description = FormatString(data.Description);
-        this.BuildType = FormatString(data.BuildType);
-        this.ElementType = FormatString(data.ElementType);
-        this.GameType = FormatString(data.GameType);
-        this.Name = FormatString(data.Name);
-        this.NumberOfPlayers = data.NumberOfPlayers;
-        this.Team = FormatString(data.team);
-        this.TeamType = FormatString(data.TeamType);
-        this.TeamSlots = data.TeamSlots;
-    }
-}
-
-
-class GameObject {
-    constructor() {
-        this.Game = undefined;
-        this.StartedDatetime = new Date();
-        this.GameType = undefined;
-        this.BuildType = undefined;
-        this.ElementType = undefined;
-        this.SubElementType = undefined;
-        this.TeamType = undefined;
+module.exports = () => {
+    class ListObject {
+        constructor(data) {
+            this.Name = FormatString(data.Name);
+            this.Description = FormatString(data.Description);
+            this.GameRules = FormatString(data.GameRules);
+            this.IsSupplemental = FormatBool(data.IsSupplemental);
+            this.IsAddon = FormatBool(data.IsAddon);
+            this.Type = FormatString(data.Type);
+        }
     }
 
-}
+    class MagicTerm {
+        constructor(data) {
+            this.Name = FormatString(data.Name);
+            this.Description = FormatString(data.Description);
+        }
+    }
+
+    class RollingChart {
+        constructor(data) {
+            this.Id = data.Id;
+            this.Name = FormatString(data.Name);
+            this.Chance = data.Chance;
+            this.ChartType = FormatString(data.ChartType);
+            this.Announcement = FormatString(data.Announcement);
+            this.IsAddon = FormatBool(data.IsAddon);
+            this.IsSupplemental = FormatBool(data.IsSupplemental);
+            this.IsRollAgain = FormatBool(data.IsRollAgain);
+        }
+    }
+
+    class RandomChart {
+        constructor(data) {
+            this.Id = data.Id;
+            this.Announcement = FormatString(data.Announcement);
+            this.Chance = data.Chance;
+            this.Description = FormatString(data.Description);
+            this.BuildType = FormatString(data.BuildType);
+            this.ElementType = FormatString(data.ElementType);
+            this.GameType = FormatString(data.GameType);
+            this.Name = FormatString(data.Name);
+            this.NumberOfPlayers = data.NumberOfPlayers;
+            this.Team = FormatString(data.team);
+            this.TeamType = FormatString(data.TeamType);
+            this.TeamSlots = data.TeamSlots;
+        }
+    }
 
 
-function FormatString(data){
-    if (data == undefined) {return "";}
-    return data;
-}
-function FormatBool(data){
-    if (data == undefined) { return false; }
-    return data;
-}
+    class GameObject {
+        constructor() {
+            this.Game = undefined;
+            this.StartedDatetime = new Date();
+            this.GameType = undefined;
+            this.BuildType = undefined;
+            this.ElementType = undefined;
+            this.SubElementType = undefined;
+            this.TeamType = undefined;
+        }
+
+    }
+
+
+    function FormatString(data) {
+        if (data == undefined) { return ""; }
+        return data;
+    }
+
+    function FormatBool(data) {
+        if (data == undefined) { return false; }
+        return data;
+    }
+};
